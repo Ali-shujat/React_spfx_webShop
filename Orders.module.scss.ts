2bcf3ddf2d0c9cb3b9b49538ea35e5e250ee5e88
Shujat Ali <34058528+Ali-shujat@users.noreply.github.com>
2019-03-04 23:01:36 +0100

    order detail

---
+++ Orders.module.scss.ts
@@ -0,0 +1,22 @@
+/* tslint:disable */
+require("./Orders.module.css");
+const styles = {
+  Orders: 'Orders_e8646f42',
+  Cards: 'Cards_e8646f42',
+  EmptyOrder: 'EmptyOrder_e8646f42',
+  OrderItem: 'OrderItem_e8646f42',
+  CancelOrder: 'CancelOrder_e8646f42',
+  Card: 'Card_e8646f42',
+  Checkout: 'Checkout_e8646f42',
+  CardItem: 'CardItem_e8646f42',
+  CardImage: 'CardImage_e8646f42',
+  CardInfo: 'CardInfo_e8646f42',
+  OrderButtons: 'OrderButtons_e8646f42',
+  Modal: 'Modal_e8646f42',
+  Backdrop: 'Backdrop_e8646f42',
+  addForm: 'addForm_e8646f42',
+  AddItemButton: 'AddItemButton_e8646f42',
+};
+
+export default styles;
+/* tslint:enable */
